032d8a3326dd7015d6f03d0e614a412bb30527fd
Kavindu Chamodh Nethsara <72559423+ChamodhNethsara@users.noreply.github.com>
2020-10-10 21:11:10 +0530

    Update unblock_vimeo.py

--- unblock_vimeo.py
+++ unblock_vimeo.py
@@ -1,12 +1,14 @@
 from subprocess import call
+from os import path
 
 
 print("\n\t Welcome to tool to unblock vimeo privacy block\n\tBy kavindu Chamodh")
-url = input("enter the url of the video that you want to get unblocked")
-permitted_url = input("Enter a url of a website that allows you to see the video ")
-filename = input("Enter a file name to write your output")+'.html'
+url = input("\nEnter the url of the video that you want to get unblocked")
+permitted_url = input("\nEnter a url of a website that allows you to see the video")
+filename = input("\nEnter a file name to write your output")+'.html'
+
 
 call('curl --referer '+permitted_url+' -o '+filename+' '+url,shell=True)
 
-#test_yrl = https://player.vimeo.com/video/109727604
-#test_urk = https://cybrary.it/
+
+print('\nOUTPUT SUCCESFULLY SAVED TO '+path.abspath(filename))
